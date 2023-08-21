701f4976a2c2c1b9d4723242d5a10f6f108bc377
ducthinh <huynhthinh61@gmail.com>
2023-08-21 20:33:57 +0700

    baitapif4

---
+++ ChiSoCoThe/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace BMI 
+{
+    class Program 
+    {
+        public static void Main(string[] args)
+        {
+                //CrVAR
+            double bmi;
+            double height;
+            double weight;
+
+                //INPUT 
+            Console.Write("height : ");
+            height = float.Parse(Console.ReadLine());
+            Console.Write("weight : ");
+            weight = float.Parse(Console.ReadLine());
+
+                //CalBMI
+            bmi = weight / Math.Pow(height,2);
+            bmi = Math.Round(bmi, 1);
+                //OUTPUT
+            Console.Write("BMI: "+ bmi);
+
+            if (bmi < 18)
+                Console.WriteLine("Underweight");
+            else if (bmi < 25.0)
+                Console.WriteLine("Normal");
+            else if (bmi < 30.0)
+                Console.WriteLine("Overweight");
+            else
+                Console.WriteLine("Obese");
+        }
+    }
+}
