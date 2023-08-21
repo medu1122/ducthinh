13862b62deb185ee58e2e454121900ad8c13fc1e
ducthinh <huynhthinh61@gmail.com>
2023-08-21 08:07:04 +0700

    toantu

---
+++ ToanTu/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace DienTich
+{
+    class Program 
+    {
+        public static void Main(string[] args)
+        {
+            float height;
+            float width;
+            Console.Write("enter width : ");
+            width =float.Parse(Console.ReadLine());
+            Console.Write("enter height : ");
+            height =float.Parse(Console.ReadLine());
+            float area = height * width ;
+            Console.WriteLine("area is : "+area);
+        }
+    }
+}
