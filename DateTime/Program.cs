9738535047187b1915c5f2fe7092d3182cbd9338
ducthinh <huynhthinh61@gmail.com>
2023-08-21 07:57:30 +0700

    datetime helloworld

---
+++ DateTime/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Threading.Tasks.Dataflow;
+
+namespace program 
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            DateTime localDate = DateTime.Now;
+            System.Console.WriteLine("date time now is : "+localDate);
+        }
+    }
+}
