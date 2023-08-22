e0b7e49e81b8da15fdf0c4f6d0e35b7cd13c52bb
ducthinh <huynhthinh61@gmail.com>
2023-08-22 11:29:42 +0700

    sua

--- bai1DIeuKienIF/Program.cs
+++ bai1DIeuKienIF/Program.cs
@@ -10,8 +10,22 @@ namespace PtrinhBac1
             double a = Convert.ToDouble(Console.ReadLine());
             Console.Write("enter B: ");
             double b = Convert.ToDouble(Console.ReadLine());
-            Console.Write("X= -B/A ="+-b+"/"+a+" = "+ -b/a);
             
+            if (a!=0&&b!=0)
+            {
+                Console.Write("X= -B/A ="+-b+"/"+a+" = "+ -b/a);
+            }
+            else 
+            {
+                if (b==0)
+                {
+                    Console.WriteLine("all x is solution!");
+                }
+                else if (a==0)
+                {
+                    Console.WriteLine("Nod solution!");
+                }
+            }
         }
     }
 }
