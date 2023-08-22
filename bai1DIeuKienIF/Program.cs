0d8f1bd971c82d90f77955c8d258408822f4cb48
ducthinh <huynhthinh61@gmail.com>
2023-08-22 11:20:48 +0700

    d

---
+++ bai1DIeuKienIF/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace PtrinhBac1 
+{
+    class Program 
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Ax+B=0");
+            Console.Write("enter A : ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("enter B: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("X= -B/A ="+-b+"/"+a+" = "+ -b/a);
+            
+        }
+    }
+}
